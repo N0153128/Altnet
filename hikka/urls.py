2879a54db8ee644638ffea69cca0827c0a5c56da
Noise <noisebro@hotmail.com>
2022-08-15 01:27:32 +0100

    fixed url import error, fixed incorrect url patten in board/urls.py

--- hikka/urls.py
+++ hikka/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
@@ -22,7 +21,7 @@ from django.urls import re_path
 from django.views.static import serve
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
     path('', include('paper.urls')),
     path('board/', include('board.urls')),
     path('manager/', include('manager.urls')),
--- manager/urls.py
+++ manager/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 app_name = 'Manager'
 urlpatterns = [
-    path('auth/', views.authsh, name='auth'),
+    path('auth/', views.authsh.as_view(), name='auth'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('me/', views.meview, name='me'),
