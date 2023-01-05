62bb126c3fc1374ca145113fa67bed058aee8285
N0153128 <noisebro@hotmail.com>
2023-01-05 14:38:19 +0000

    Added more Errors localisation

--- scripts/loc/Errors.py
+++ scripts/loc/Errors.py
@@ -12,6 +12,14 @@ ILLEGAL_ACTION_AGENDA_POST_REMOVE = ['You cannot remove this agenda post', 'Вы
 ILLEGAL_ACTION_PUBLIC_MESSAGES_REMOVE = ['You cannot remove this public post', 'Вы не можете удалить этот пост']
 ILLEGAL_ACTION_LANGUAGE_CODE = ['This is unsupported language code', 'Этот языковой код не поддерживается']
 ILLEGAL_ACTION_FILE_TYPE = ['We don\'t use this file type', 'Мы не используем этот формат, бро']
+ILLEGAL_ACTION_HOST_ONLY = ['Only hosts can do that', 'Действие доступно только хостам']
+ILLEGAL_ACTION_ROOM_ALREADY_HIDDEN = ['The room is already hidden', 'Комната уже скрыта']
+ILLEGAL_ACTION_AUTOPLAY_ALREADY_ON = ['The autoplay is already on', 'Автовоспроизведение уже включено']
+ILLEGAL_ACTION_AUTOPLAY_ALREADY_OFF = ['The autoplay is already off', 'Автовоспроизведение уже выключено']
+ILLEGAL_ACTION_TOLERANCE_ALREADY_OFF = ['The room is already media tolerant', 'Показ медиа уже запрещён']
+ILLEGAL_ACTION_TOLERANCE_ALREADY_ON = ['The room is already media intolerant', 'Показ медиа уже разрешен']
+ILLEGAL_ACTION_ROOM_USER_ABSENT = ['Selected user is not present in the room', 'Выбранный пользователь не в комнате']
+
 
 REF_KEY_NOT_EXISTING = ['This key doesn\'t exist', 'Введённый ключ не существует']
 REF_KEY_OUTDATED = ['This key is outdated', 'Ключ устарел']
